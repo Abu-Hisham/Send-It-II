3c814b5b9d5a58a0befb459740b4bec7483a8f87
Abdulaziz <abdulmoha786@gmail.com>
2018-11-08 17:06:14 +0300

    [ft- add parcel model and its members-#161732451]

--- Send-It-II/app/api/V1/models.py
+++ Send-It-II/app/api/V1/models.py
@@ -53,3 +53,66 @@ class User (object):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
+class Parcel (object):
+    """"Class Description goes here"""
+    parcel_list=[]
+
+    def __init__(self, sender_name,sender_phone, sender_location, recipient_name, recipient_phone, recipient_location):
+        self.parcel_id = len(Parcel.parcel_list) + 1
+        self.sender_name = sender_name
+        self.sender_phone = sender_phone
+        self.sender_location = sender_location
+        self.recipient_name = recipient_name
+        self.recipient_phone = recipient_phone
+        self.recipient_location = recipient_location
+        self.status = ""
+
+    def changeStatus(self, new_status):
+        self.status = new_status
+        return self.status
+
+    def changeLocation(self, new_location):
+        self.recipient_location = new_location
+        return self.recipient_location
+    
+    @staticmethod
+    def addParcel(parcel):
+        Parcel.parcel_list.append(parcel)
+
+    @staticmethod
+    def removeParcel(parcel_id):
+        parcel = Parcel.getParcel(parcel_id)
+        Parcel.parcel_list.remove(parcel)
+
+    @staticmethod
+    def getParcel(parcel_id):
+        for parcel in Parcel.parcel_list:
+            if parcel.parcel_id == parcel_id:
+                return parcel
+        return None
+
+    def __eq__(self, other):
+        return self.email == other.email
+
+    def __iter__(self):
+        self.__index = -1
+        return self
+
+    def __next__(self):
+        if self.__index >= len(Parcel.parcel_list) - 1:
+            raise StopIteration
+        self.__index += 1
+        parcel = Parcel.parcel_list[self.__index]
+        return parcel
+
+    def __repr__(self):
+        return {'parcel_id':self.parcel_id_id,
+                'sender_name': self.sender_name,
+                'sender_phone':self.sender_phone,
+                'sender_location':self.sender_location,
+                'recipient_name':self.recipient_name,
+                'recipient_phone':self.recipient_phone,
+                'recipient_location':self.recipient_location,
+                'parcel_status':self.status
+                }
