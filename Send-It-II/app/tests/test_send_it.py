7931b44b2d968b0210b1ee59b45f45a9b0a081da
Abdulaziz <abdulmoha786@gmail.com>
2018-11-09 09:39:14 +0300

    [ft- set test variables-#161732128]

--- Send-It-II/app/tests/test_send_it.py
+++ Send-It-II/app/tests/test_send_it.py
@@ -12,6 +12,12 @@ class WeconnectTestCase (unittest.TestCase):
         """Define variables and initialize app."""
         self.client = app.test_client ()
         self.client.testing = True
+        self.parcel_list = []
+        self.user_list = []
+        self.user1 = User(1,"Abdul","abdul@gmail.com","0701633016","mypass")
+        self.user2 = User(2,"Zahry","zahry@gmail.com","0702733016","mypass")
+        self.parcel1 = Parcel("Abdul","0701633016","Madaraka","Zahry","0702733016","Kileleshwa")
+        self.parcel2 = Parcel("Zahry", "0702733016", "Kileleshwa", "Abdul", "0701633016", "Madaraka")
     def test_get_a_single_parcel(self):
         pass
     def test_get_all_parcels(self):
